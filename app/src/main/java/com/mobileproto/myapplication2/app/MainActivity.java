df42318e62e62272993a70f42350bbfd9b628673
Chris <sihrc.c.lee@gmail.com>
2014-05-26 10:42:27 -0700

    Added comments to the code

--- app/src/main/java/com/mobileproto/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/mobileproto/myapplication2/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +12,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Sets this activity's content view to the activity_main.xml layout
+
+        setupViews(); //Sets up views in this content view.
+    }
+
+    private void setupViews(){
+        TextView text = (TextView) findViewById(R.id.main_hello_text); //Find the TextView from the ContentView by ID.
+        text.setOnClickListener(OnClickListeners.helloWorldListener); //Set OnClick Listener for TextView click functionality
     }
 
 
