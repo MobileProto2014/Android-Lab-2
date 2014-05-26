bb2a734638b4592abebaf30902efc725f9947a7a
Chris <sihrc.c.lee@gmail.com>
2014-05-26 15:53:00 -0700

    renamed module and refactored the packages

--- chatbox/src/main/java/com/mobileproto/chatbox/MainActivity.java
+++ chatbox/src/main/java/com/mobileproto/chatbox/MainActivity.java
@@ -1,15 +1,15 @@
-package com.mobileproto.myapplication2.app;
+package com.mobileproto.chatbox;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ chatbox/src/main/java/com/mobileproto/chatbox/OnClickListeners.java
@@ -0,0 +1,10 @@
+package com.mobileproto.myapplication2.chatbox;
+
+/**
+ * Created by clee2 on 5/26/2014.
+ */
+public class OnClickListeners {
+    /**
+     * MainActivity OnClickListeners
+     */
+}
