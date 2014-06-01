cfa3aa0ba46d2f54634eb1bd63f1970b0451330d
Chris <sihrc.c.lee@gmail.com>
2014-05-31 22:03:18 -0700

    Added channel object

--- chatbox/src/main/java/com/mobileproto/chatbox/models/Channel.java
+++ chatbox/src/main/java/com/mobileproto/chatbox/models/Channel.java
@@ -4,4 +4,25 @@ package com.mobileproto.chatbox.models;
  * Created by clee2 on 5/31/2014.
  */
 public class Channel {
+    private String id, name;
+    public Channel(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+//    public void setId(String newId){
+//        this.id = newId;
+//    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
 }
